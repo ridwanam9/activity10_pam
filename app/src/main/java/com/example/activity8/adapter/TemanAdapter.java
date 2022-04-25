fcbc146d56a2443814f4aee0d7e93cc547a72312
ridwanam9 <ridwanamirulmaulana8@gmail.com>
2022-04-25 09:47:21 +0700

    Initial commit

---
+++ app/src/main/java/com/example/activity8/adapter/TemanAdapter.java
@@ -0,0 +1,72 @@
+package com.example.activity8.adapter;
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.activity8.R;
+import com.example.activity8.database.Teman;
+
+
+import java.util.ArrayList;
+
+public class TemanAdapter extends RecyclerView.Adapter<TemanAdapter.TemanViewHolder>{
+
+    private ArrayList<Teman> listData;
+    private Context control;
+
+    public TemanAdapter(ArrayList<Teman> listData) {
+        this.listData = listData;
+    }
+
+    @Override
+    public TemanViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater layoutInf = LayoutInflater.from(parent.getContext());
+        View view = layoutInf.inflate(R.layout.row_data_teman,parent,false);
+        control = parent.getContext();
+        return new TemanViewHolder(view);
+    }
+    @Override
+    public void onBindViewHolder(TemanViewHolder holder, int position) {
+
+        String nm,tlp,id;
+
+        id = listData.get(position).getId();
+        nm = listData.get(position).getNama();
+        tlp = listData.get(position).getTelpon();
+
+
+        holder.namaTxt.setText(nm);
+        holder.namaTxt.setTextSize(30);
+        holder.namaTxt.setTextColor(Color.BLUE);
+        holder.telponTxt.setText(tlp);
+
+        holder.cardku.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return (listData != null)? listData.size() : 0;
+    }
+
+    public class TemanViewHolder extends RecyclerView.ViewHolder {
+        private CardView cardku;
+        private TextView namaTxt, telponTxt;
+        public TemanViewHolder(View view) {
+            super(view);
+            cardku = (CardView)view.findViewById(R.id.kartuku);
+            namaTxt = (TextView) view.findViewById(R.id.textNama);
+            telponTxt = (TextView) view.findViewById(R.id.textTelpon);
+        }
+    }
+}
