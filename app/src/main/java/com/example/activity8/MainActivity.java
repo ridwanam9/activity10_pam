d9ba63fb262d299334c2ad2ea55713432f15dd20
ridwanam9 <ridwanamirulmaulana8@gmail.com>
2022-05-25 11:53:35 +0700

    Initial Commit

--- app/src/main/java/com/example/activity8/MainActivity.java
+++ app/src/main/java/com/example/activity8/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
     private FloatingActionButton fab;
 
     private static final String TAG = MainActivity.class.getSimpleName();
-    private static String url_select = "https://10.0.2.2/umyTI/bacateman.php";
+    private static String url_select = "https://20200140041.praktikumtiumy.com/bacateman.php";
     public static final String TAG_ID = "id";
     public static final String TAG_NAMA = "nama";
     public static final String TAG_TELPON = "telpon";
